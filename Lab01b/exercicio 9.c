acaae863ec4cb736187c94828b518cada2e36687
Junkkeira <94480584+Junkkeira@users.noreply.github.com>
2023-02-23 16:17:17 -0300

    Delete exercicio 9.c

--- Lab01b/exercicio 9.c
+++
@@ -1,8 +0,0 @@
-void subNegMod(int m[][C], int lin) {
-    for (int i = 0; i < lin; i++) {
-        for (int j = 0; j < C; j++) {
-            if (m[i][j] < 0)
-                m[i][j] = abs(m[i][j]);
-        }
-    }
-}
