8aeacbad5f7d28d8762f94c253c801ebaf775691
Felipe Clé <felipee.cle@gmail.com>
2023-02-23 15:27:30 -0300

    Update exercicio 14

--- Lab01b/exercicio14.c
+++ Lab01b/exercicio14.c
@@ -5,7 +5,6 @@ int main()
 {
     int i ;
     char str[100];
-    char str1[100];
     char ch;
     printf("Digite uma string : ");
     scanf("%s",str);
@@ -13,11 +12,11 @@ int main()
     scanf("%d",&i);
     printf("Digite o caracter a ser inserido :");
     scanf(" %c",&ch);
-    str1[i] = ch;
-    for(int i = 0; i < 100; ) {
-    char temp = str[i++];
-    str1[i] = temp;
-  }
-    printf("%s", str1);    
+    for (int j = 0 ; j<100 ; j++){
+      if (str[j] == str[i]){
+        str[j]= ch;
+      }
+    }
+    printf("%s", str);    
     
 }
