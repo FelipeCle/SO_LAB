99f5959ee7d016cda7d07934c38879d70d5b4fdb
Victor Junqueira <94480584+Junkkeira@users.noreply.github.com>
2023-03-16 17:08:11 -0300

    Update fork-question1.c

--- Lab02b/fork-question1.c
+++ Lab02b/fork-question1.c
@@ -3,23 +3,34 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int value = 20;
+int main(){
+int fd[2];
+pid_t pid;
+int value = 5;
 
-int main()
-{
-	pid_t pid;
-
-	pid = fork();
-
-	if (pid == 0) { /* child process */
-		printf("Entrei no filho!\n");
-		value += 15;
-		printf ("CHILD: value = %d\n",value); /* LINE A */
-		return 0;
-	}
-	else if (pid > 0) { /* parent process */
-		wait(NULL);
-		printf ("PARENT: value = %d\n",value); /* LINE A */
-		return 0;
-	}
+if (pipe(fd) == -1) { // creating pipe
+  printf("Error creating pipe\n");
+  return 1;
 }
+
+pid = fork();
+
+if (pid == 0) { // child process
+  close(fd[0]); // close read end of pipe
+  value += 15;
+  write(fd[1], &value, sizeof(value)); // write value to pipe
+  close(fd[1]); // close write end of pipe
+  return 0;
+}
+else if (pid > 0) { // parent process
+  close(fd[1]); // close write end of pipe
+  wait(NULL);
+  read(fd[0], &value, sizeof(value)); // read value from pipe
+  close(fd[0]); // close read end of pipe
+  printf("PARENT: value = %d\n", value);
+  return 0;
+}
+  else { // error creating child process
+  printf("Error creating child process\n");
+  return 1;
+}}
