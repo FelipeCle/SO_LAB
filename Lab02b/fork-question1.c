28d161e7497c420ba7373ef87538a60873f6930a
Victor Junqueira <94480584+Junkkeira@users.noreply.github.com>
2023-03-16 17:13:41 -0300

    Update fork-question1.c

--- Lab02b/fork-question1.c
+++ Lab02b/fork-question1.c
@@ -8,29 +8,29 @@ int fd[2];
 pid_t pid;
 int value = 5;
 
-if (pipe(fd) == -1) { // creating pipe
-  printf("Error creating pipe\n");
-  return 1;
+if (pipe(fd) == -1) { // criando pipe
+    printf("Erro ao criar o pipe\n");
+    return 1;
 }
 
 pid = fork();
 
-if (pid == 0) { // child process
-  close(fd[0]); // close read end of pipe
-  value += 15;
-  write(fd[1], &value, sizeof(value)); // write value to pipe
-  close(fd[1]); // close write end of pipe
-  return 0;
+if (pid == 0) { // processo filho
+    close(fd[0]); // fechar a extremidade de leitura do pipe
+    value += 15;
+    write(fd[1], &value, sizeof(value)); // escrever o valor no pipe
+    close(fd[1]); // fechar a extremidade de escrita do pipe
+    return 0;
 }
-else if (pid > 0) { // parent process
-  close(fd[1]); // close write end of pipe
-  wait(NULL);
-  read(fd[0], &value, sizeof(value)); // read value from pipe
-  close(fd[0]); // close read end of pipe
-  printf("PARENT: value = %d\n", value);
-  return 0;
+else if (pid > 0) { // processo pai
+    close(fd[1]); // fechar a extremidade de escrita do pipe
+    wait(NULL);
+    read(fd[0], &value, sizeof(value)); // ler o valor do pipe
+    close(fd[0]); // fechar a extremidade de leitura do pipe
+    printf("PARENT: value = %d\n", value);
+    return 0;
 }
-  else { // error creating child process
-  printf("Error creating child process\n");
-  return 1;
+else { // erro ao criar processo filho
+    printf("Erro ao criar processo filho\n");
+    return 1;
 }}
